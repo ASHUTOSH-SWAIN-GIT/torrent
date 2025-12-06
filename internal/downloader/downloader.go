8df26da70d7eccac9820f1b9fffbc95a9a9e8638
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-12-06 06:50:45 +0530

    download pices from multiple peers concurrently

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -1,6 +1,7 @@
 package downloader
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"net"
 	"os"
@@ -80,20 +81,89 @@ func (s *Session) DownloadAll(peers []tracker.Peer) error {
 			continue
 		}
 		fmt.Println("ok")
+
 		conns = append(conns, conn)
-
-		if len(conns) == 0 {
-			return fmt.Errorf("could not connect to any peers")
-		}
-
-		fmt.Println("Connected peers:", len(conns))
-
-		// create  piece queue
-		pieceCh := make(chan int, numPieces)
-
-		for i := 0; i < numPieces; i++ {
-			pieceCh <- 1
-		}
-		close(pieceCh)
 	}
+
+	if len(conns) == 0 {
+		return fmt.Errorf("could not connect to any peers")
+	}
+
+	fmt.Println("Connected peers:", len(conns))
+
+	// create piece queue
+	pieceCh := make(chan int, numPieces)
+	for i := 0; i < numPieces; i++ {
+		pieceCh <- i
+	}
+	close(pieceCh)
+
+	var wg sync.WaitGroup
+	errCh := make(chan error, len(conns))
+
+	// worker for each connection
+	for _, c := range conns {
+		wg.Add(1)
+		go func(conn net.Conn) {
+			defer wg.Done()
+			defer conn.Close()
+
+			for pieceIndex := range pieceCh {
+				if err := s.downloadoOnePieceFromPeer(conn, pieceIndex); err != nil {
+					fmt.Println("Error downloading piece", pieceIndex, "from peer", err)
+					errCh <- err
+					return
+				}
+			}
+		}(c)
+	}
+
+	wg.Wait()
+
+	// after all workers are done
+	select {
+	case err := <-errCh:
+		return err
+	default:
+		return nil
+	}
+}
+
+// download  a single  piece from the peer over one connection
+func (s *Session) downloadoOnePieceFromPeer(conn net.Conn, pieceIndex int) error {
+	pieceLen := s.Torrent.Info.PieceLength
+
+	if pieceIndex == s.Torrent.NumPieces-1 {
+		totalLen := s.Torrent.Info.Length
+		remaining := totalLen - pieceIndex*s.Torrent.Info.PieceLength
+		pieceLen = remaining
+	}
+
+	fmt.Println("Peer ", conn.RemoteAddr(), "is downloading piece", pieceIndex)
+
+	data, err := peer.DownloadPiece(conn, pieceIndex, pieceLen)
+	if err != nil {
+		return err
+	}
+
+	expected := pieceHash(s.Torrent, pieceIndex)
+	actual := sha1.Sum(data)
+
+	if expected != actual {
+		return fmt.Errorf("piece %d failed hash check", pieceIndex)
+	}
+
+	// write to file
+	offset := int64(pieceIndex * s.Torrent.Info.PieceLength)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, err = s.file.WriteAt(data, offset)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Piece", pieceIndex, "done")
+	return nil
+
 }
--- main.go
+++ main.go
@@ -1,42 +1,54 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
-	"net"
 	"os"
 
-	"torrent.ashutosh.net/internal/peer"
+	"torrent.ashutosh.net/internal/downloader"
 	"torrent.ashutosh.net/internal/tracker"
 )
 
 func main() {
 
+	// load torrent file
 	data, err := os.ReadFile("cab4A8WPHeOLLZvbDYW4wySxq4CNFexA1KHNoT8O.torrent")
 	if err != nil {
 		panic(err)
 	}
 
-	torrent, err := tracker.ParseTorrent(data)
+	// parse torrent metadata
+	t, err := tracker.ParseTorrent(data)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Announce:", torrent.Announce)
-	fmt.Println("Info Hash:", torrent.InfoHash)
+	fmt.Println("Announce:", t.Announce)
+	fmt.Println("Info Hash:", t.InfoHash)
+	fmt.Println("Name:", t.Info.Name)
+	fmt.Println("Piece Length:", t.Info.PieceLength)
+	fmt.Println("Total Length:", t.Info.Length)
+	fmt.Println("Total Pieces:", t.NumPieces)
 
+	// generate peer id
 	peerID := tracker.GeneratePeerID()
 	fmt.Println("Peer ID:", string(peerID[:]))
 
-	announceURL, err := tracker.BuildAnnounceURL(torrent, peerID, 6881)
+	// build announce url
+	announceURL, err := tracker.BuildAnnounceURL(t, peerID, 6881)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Announce URL:", announceURL)
 
-	peers, err := tracker.Announce(announceURL, torrent, peerID, 6881)
+	// request tracker for peers
+	peers, err := tracker.Announce(announceURL, t, peerID, 6881)
 	if err != nil {
 		panic(err)
+	}
+
+	if len(peers) == 0 {
+		fmt.Println("Tracker returned no peers")
+		return
 	}
 
 	fmt.Println("\nPeers returned by tracker:")
@@ -44,78 +56,20 @@ func main() {
 		fmt.Println(p.IP, p.Port)
 	}
 
-	fmt.Println("\nTrying to find one peer for downloading...")
-
-	var conn net.Conn
-	var workingPeer tracker.Peer
-	found := false
-
-	for _, p := range peers {
-		fmt.Printf("Trying %s:%d ... ", p.IP, p.Port)
-
-		c, err := peer.ConnectAndHandshake(p, torrent.InfoHash, peerID)
-		if err != nil {
-			fmt.Println("Failed:", err)
-			continue
-		}
-
-		fmt.Println("Connected")
-		conn = c
-		workingPeer = p
-		found = true
-		break
-	}
-
-	if !found {
-		fmt.Println("Could not connect to any peer")
-		return
-	}
-
-	defer conn.Close()
-
-	fmt.Println("\nConnected to peer:", workingPeer.IP, workingPeer.Port)
-
-	// Download first piece
-	pieceIndex := 0
-	pieceLength := torrent.Info.PieceLength
-
-	// Handle last piece length
-	if pieceIndex == len(torrent.Info.Pieces)/20-1 {
-		remaining := torrent.Info.Length - pieceIndex*pieceLength
-		pieceLength = remaining
-	}
-
-	fmt.Println("Downloading piece", pieceIndex)
-
-	pieceData, err := peer.DownloadPiece(conn, pieceIndex, pieceLength)
+	// create download session
+	outputPath := "downloaded_" + t.Info.Name
+	sess, err := downloader.NewSession(t, peerID, outputPath)
 	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\nStarting multi peer download for", t.Info.Name)
+
+	// start downloading all pieces
+	if err := sess.DownloadAll(peers); err != nil {
 		fmt.Println("Download failed:", err)
 		return
 	}
 
-	// Extract expected hash from pieces
-	var expectedHash [20]byte
-	if pieceIndex*20+20 <= len(torrent.Info.Pieces) {
-		copy(expectedHash[:], torrent.Info.Pieces[pieceIndex*20:pieceIndex*20+20])
-	} else {
-		fmt.Println("Invalid piece index")
-		return
-	}
-	actualHash := sha1.Sum(pieceData)
-
-	if expectedHash != actualHash {
-		fmt.Println("Piece verification failed")
-		return
-	}
-
-	fmt.Println("Piece verified")
-
-	// Save piece to file
-	err = os.WriteFile("output_piece.bin", pieceData, 0644)
-	if err != nil {
-		fmt.Println("Failed to save piece", err)
-		return
-	}
-
-	fmt.Println("Piece saved as output_piece.bin")
+	fmt.Println("\nDownload completed successfully. Saved to:", outputPath)
 }
